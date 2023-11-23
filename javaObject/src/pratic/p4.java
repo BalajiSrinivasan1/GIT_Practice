c5f1a411bbe2e88aa4a05d5ba98490556b74a923
HP <HP@DESKTOP-EGOL3EF>
2023-11-23 21:15:59 +0530

    java program

--- javaObject/src/pratic/p4.java
+++ javaObject/src/pratic/p4.java
@@ -6,9 +6,15 @@ public class p4 {
 		// TODO Auto-generated method stub
 		int n=3;
 		for(int i=1;i<=n;i++) {
+			int num=1;
 			for(int j=1;j<n*2;j++) {
 				if(i+j>n&&j-i<n) {
-					System.out.print(j+" ");
+					System.out.print(num+" ");
+					if(j>=n) {
+						num--;
+					}else {
+						num++;
+					}
 				}else { 
 					System.out.print("  ");
 				}
